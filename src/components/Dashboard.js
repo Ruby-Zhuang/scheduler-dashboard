b1de3f255901422b492edc67a0bcc6d4ab9414e3
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-20 12:54:55 -0400

    Setup project and iadd Loading component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,20 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Loading from 'components/Loading';
 
-import classnames from "classnames";
+import classnames from 'classnames';
 
 class Dashboard extends Component {
+  state = { loading: true };
+  /*
+   * The only function that a component needs to declare is the render function.
+   * This render function is equivalent to the body of the components that we declare as functions.
+   */
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames('dashboard');
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
