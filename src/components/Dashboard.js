2643c7344fc59e076b521a3faabee81d707865c7
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-20 13:09:45 -0400

    Setup state to handle a focused view feature

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,26 +29,35 @@ const data = [
 ];
 
 class Dashboard extends Component {
-  state = { loading: false };
+  state = { loading: false, focused: null };
   /*
    * The only function that a component needs to declare is the render function.
    * This render function is equivalent to the body of the components that we declare as functions.
    */
   render() {
-    const dashboardClasses = classnames('dashboard');
+    const dashboardClasses = classnames('dashboard', {
+      'dashboard--focused': this.state.focused,
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map((panel) => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
-    ));
+    // When the value is null it means we are in the unfocused four-panel view.
+    // When we are in focused mode, we don't want to render four panels; we want to render one.
+    const panels = data
+      .filter(
+        (panel) =>
+          this.state.focused === null || this.state.focused === panel.id
+      )
+      .map((panel) => (
+        <Panel
+          key={panel.id}
+          id={panel.id}
+          label={panel.label}
+          value={panel.value}
+        />
+      ));
 
     return <main className={dashboardClasses}>{panels}</main>;
   }
